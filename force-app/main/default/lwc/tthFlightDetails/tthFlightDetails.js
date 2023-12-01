61143e65279600d45ab7ec3514788872e7d1fcf9
Leela Mohan <leelamohan.g@gmail.com>
2023-12-01 17:14:41 +0530

    git branch -M main

--- force-app/main/default/lwc/tthFlightDetails/tthFlightDetails.js
+++ force-app/main/default/lwc/tthFlightDetails/tthFlightDetails.js
@@ -9,7 +9,7 @@ import My_Resource from '@salesforce/resourceUrl/TTHAIRAssetFlow';
 
 export default class TthFlightDetails extends LightningElement {
 
-    @track flightData;
+    @track flightData = [];
     flightJourneyImg = My_Resource + '/travelingwithpet/screen3_image.png';
 
     @api recordId;
@@ -22,11 +22,17 @@ export default class TthFlightDetails extends LightningElement {
         data
     }) {
         if (data) {
+            console.log('*** TTH FLIGHT DETAILS ***');
             console.log(data);
             this.flightData = data;
         } else if (error) {
             console.error('Error fetching flight data:', error);
         }
+    }
+
+    @api
+    get isFlightDetailsAvailable() {
+        return this.flightData.length > 0 ? true : false;
     }
 
 }
